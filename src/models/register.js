ac9d0b6011399bc67b9321cb9ce678de2820432c
Rajbir <rbsinghk30@gmail.com>
2022-04-26 11:20:20 +0530

    Bcrypt add

--- src/models/register.js
+++ src/models/register.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt")
 const new_mongoose = new mongoose.Schema({
     name:{
         type:String,
@@ -15,7 +15,16 @@ const new_mongoose = new mongoose.Schema({
         required:true,
         unique:true
     },
-    
+    password:{
+        type:String,
+        required:true
+    },
 });
+    new_mongoose.pre("save", async function(next){
+        if(this.isModified("password")){
+            this.password = await bcrypt.hash(this.password,10);
+        }
+    next();
+    })    
 const register = new mongoose.model("register", new_mongoose);
 module.exports=register;
