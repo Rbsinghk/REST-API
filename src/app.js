ea2f73ad67114154de299cf0638f97600dc149cd
Rajbir <rbsinghk30@gmail.com>
2022-04-29 01:04:54 +0530

    .

--- src/app.js
+++ src/app.js
@@ -5,22 +5,11 @@ const { default: mongoose } = require("mongoose");
 const app = express();
 require("./db/conn");
 const register = require("./models/register");
+const bcrypt = require("bcrypt")
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/register", async (req,res)=>{
-    try {
-        const new_register = new register(req.body);
-        const token = await new_register.generateAuthToken()
-        const reg = await new_register.save();
-
-        res.status(201).send(reg);
-    } catch (error) {
-        res.status(400).send(error)
-    }
-    
-})
 app.get("/register", async (req,res)=>{
     try {
         const get = await register.find({});
@@ -61,6 +50,42 @@ app.delete("/register/:id", async (req,res)=>{
     }
 })
 
+app.post("/register", async (req,res)=>{
+    try {
+        const new_register = new register(req.body);
+        const token = await new_register.generateAuthToken()
+        const reg = await new_register.save();
+
+        res.status(201).send(reg);
+    } catch (error) {
+        res.status(400).send(error)
+    }
+    
+})
+
+app.post("/login", async (req,res)=>{
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+    
+        const usermail = await register.findOne({ email: email });
+        
+        const isMatch = await bcrypt.compare(password, usermail.password);
+        console.log(isMatch)
+        
+        const token = await usermail.generateAuthToken();
+        console.log(token);
+    
+        if (isMatch) {
+          res.status(201).send("Successfully login");
+        } else {
+          res.send("Invalid Username or Password");
+        }
+      } catch (error) {
+        res.status(400).send("Invalid email");
+      }
+    });
+    
 app.listen(port,()=>{
     console.log(`The port is running at ${port}`);
 })
